7ab8708825eecb6894efba0bec21cdd5449061f0
janetmuthoni <97959577+janetmuthoni@users.noreply.github.com>
2022-03-14 17:07:23 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,63 +1,83 @@
 /*
-    County Library System,
-    by, Francesco,
-    Feb 2022,
-    MIT License,
-    C99 Compiler,
+    County Library System
+    By Trappy
+    7th feb 2022
+    MIT license
+    C99 Compiler
 */
-
 #include <stdio.h>
 #include <stdlib.h>
+struct patron{
 
-    int menu();
+    char name[100];
+    char email[50];
+    char password[30];
+    int is_staff;
 
-int main()
-{
-    char title[100] = "County Library Management";
-    char status[100] = "welcome Francesco";
+    };
 
-    int v;
+void execute_action(int action);
+void add_patron()
+    {
+        struct patron patron;
+    printf("enter name: ");
+    getchar();
+    gets(patron.name);
+    printf("enter email");
+    gets(patron.email);
+    printf("enter initial password: ");
+    gets(patron.password);
+    printf("enter 1 if staff 0 otherwise: ");
+    scanf("%d",&patron.is_staff);
+    printf("Patron %s successfully added\n",patron.name);
 
-    printf("\n\n\n\t\t\t   ****************\n\t\t\t%s\n \t\t\t\    %s\n\t\t\t   ****************\n", title, status);
-    menu();
 
-
-
-    return 0;
 }
 
-int menu () {
-    int v;
 
-    printf("Select an action below\n");
-    printf("1. Add new Patron\n");
-    printf("2. view all patrons\n");
-    printf("3. Add new resources\n");
-    printf("4. View all resources\n");
-    printf("Your actions: \n");
-    scanf("%d", &v);
 
-    printf("Choice is: %d\n", v);
-    switch(v) {
+int menu(){
+    int action;
+        printf("Select An Action Below...\n");
+        printf("1. Add New Patron\n");
+        printf("2. View All Patrons\n");
+        printf("3. Add New Resources\n");
+        printf("4. View All Resources\n\n");
+        printf("Your Selection: \n");
+        scanf("%d", &action);
+        if (action < 1 || action > 4){
+            printf("Invalid Action. Try Again!\n\n");
+        }
+        return action;
+}
+
+void execute_action(int action) {
+    switch (action) {
     case 1:
-        printf(" Add new Patron\n");
+        add_patron();
         break;
     case 2:
-        printf(" view all patrons\n");
+        printf("Here is the list of Patron\n");
         break;
     case 3:
-        printf(" Add new resources\n");
+        printf("Adding New Resources\n");
         break;
     case 4:
-        printf(" View all resources\n");
+        printf("Here Is the list of Resources\n");
         break;
     default:
-        printf("Wrong Entry.");
+        printf("Wrong Entry\n");
 
     }
 
-    return 0;
-
-
 }
 
+int main(){
+    int action;
+    char title[100] = "COUNTY LIBRARY SYSTEM";
+    char status[100] = "Welcome Weston";
+    printf("\n\n\n\t\t\t   *********************\n\t\t\t%s\n \t\t\t    %s\n\t\t\t   ********************\n\n", title, status);
+    action = menu();
+    execute_action(menu());
+    return 0;
+}
